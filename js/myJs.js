c5934ae3a3ef09055106bb824047db0c78ff4eb1
kiimcuongdev <79624556+kimcuongdev@users.noreply.github.com>
2022-11-12 21:24:52 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -3,8 +3,8 @@ const textConfig = {
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
   text3: "Cậu yêu tớ có phải không nào ._.",
   text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
+  text6: "Cậu mơ à???",
+  text5: "Yêu ơi là yêu <3",
   text7: "lí do cậu thích tớ đi :vvvv",
   text8: "Gửi cho tớ <3",
   text9: "Vì cậu đẹp try vlllll",
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com";
+            window.location = "https://www.facebook.com/nkc9104/";
           },
         });
       }
